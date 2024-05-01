5431da8def17700ef499ada980a4f43d647b9931
Revath S Kumar <rsk@revathskumar.com>
2024-05-01 13:55:17 +0200

    string compile time concat

--- src/1_string.zig
+++ src/1_string.zig
@@ -6,3 +6,21 @@ test "simple string assignment" {
 
     try std.testing.expectEqualStrings("world", str);
 }
+
+// test "string concat" {
+//     const hello: []const u8 = "Hello";
+//     const world: []const u8 = "World";
+//     // str = "Hello";
+//     // str = "world";
+
+//     try std.testing.expectEqualStrings("Hello World", hello ++ world);
+// }
+
+test "string : array literal" {
+    const hello = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
+
+    try std.testing.expectEqualStrings("hello", &hello);
+}
+test "string : concat at comptime" {
+    try std.testing.expectEqualStrings("Hello World", "Hello" ++ " " ++ "World");
+}
